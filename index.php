ac0a794b6389b8cade37d897b35c780c8dc50b59
Akashrwt121 <akashrwt555@gmail.com>
2024-05-30 10:57:20 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,161 @@
+<div include></div>
+<div class="page-title">Attendance Management System</div>
+<hr>
+<!doctype html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Welcome to iCoder. A blog for coding enthusiasts">
+
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <title>Student Attendance Management System</title>
+</head>
+<body>
+
+<div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2" class="active"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./forever.jpg"height="750" width="800" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h2>Welcome To Student Attendance Management System </h2>    
+          <p>Good Experience With Good Visibility</p>
+          <button class="btn btn-danger"><a href=""></a></button>
+         
+          <button class="btn btn-success"><a href=""></a></button> 
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./sher .jpg" height="750" width="800" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h2>Mostly Accurate</h2>
+          <p>Good Experience With Good Visibility</p>
+          <button class="btn btn-danger"><a href=""></a></button>
+          
+          <button class="btn btn-success"><a href=""></a></button>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./about-1.jpg" height="750" width="800" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 style="color: black;">With No Error</h2>
+          <p>Good Experience With Good Visibility</p>
+          <button class="btn btn-danger"><a href=""></a></button>
+          <button class="btn btn-success"><a href=""></a></button>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  <div class="container my-4">
+    <div class="row mb-2">
+      <div class="col-md-6">
+        <div
+          class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-primary">BCA</strong>
+            <h3 class="mb-0">About BCA</h3>
+            <div class="mb-1 text-muted">feb 12</div>
+            <p class="card-text mb-auto"><b>BCA</b>, Bachelor of Computer Applications, is an undergraduate program focusing on computer science and its applications in various fields.
+
+
+
+
+            </p>
+            <a href="bca.html" class="stretched-link">Continue reading</a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <img class="bd-placeholder-img" width="200" height="250" src="./our image.jpg" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div
+          class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-success">MCA</strong>
+            <h3 class="mb-0">About MCA</h3>
+            <div class="mb-1 text-muted">Feb 12</div>
+            <p class="mb-auto">
+             <b> MCA</b>, Master of Computer Applications, is a postgraduate degree emphasizing advanced computer science concepts and practical application development skills.</p>
+            <a href="mca.html" class="stretched-link">Continue reading</a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <img class="bd-placeholder-img" width="200" height="250"
+              src="mca.jpeg" alt="">
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mb-2">
+      <div class="col-md-6">
+        <div
+          class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-danger">COMPUTER LAB</strong>
+            <h3 class="mb-0">About Computer Lab</h3>
+            <div class="mb-1 text-muted">feb 12</div>
+            <p class="card-text mb-auto">
+              A <b>computer lab</b> is a facility equipped with computers and software for educational, research, or training purposes in various disciplines.</p>
+            <a href="computer lab.html" class="stretched-link">Continue reading</a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <img class="bd-placeholder-img" width="200" height="250" src="thumb2.jpg" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div
+          class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-warning">LIBRARY</strong>
+            <h3 class="mb-0">About Library</h3>
+            <div class="mb-1 text-muted">feb 12</div>
+            <p class="mb-auto">A <b>library</b> is a resource hub providing books, digital materials, and services for research, learning, and community engagement.</p>
+            <a href="lib.html" class="stretched-link">Continue reading</a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <img class="bd-placeholder-img" width="200" height="250" src="library.jpg" alt="">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="container">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>© 2024-2025 STUDENT ATTENDANCE MANAGEMENT SYSTEM, (MAHGU)  <br> </p>
+    <P><b> By: Sheetal, Priyanshu Singh, Sachin, Akash Rawat, Gaurav, Sahil Rawat, Ayush Rawat, Kishan </b> </P>
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
